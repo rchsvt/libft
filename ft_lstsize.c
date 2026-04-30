020af4b331ef348575275ed3999ce3e654f1d8fe
remy chavastel <remy_chvst@MacBook-Pro-de-remy.local>
2026-04-30 19:38:40 +0200

    v3

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -12,22 +12,15 @@
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+int	ft_lstsize(t_list *lst)
 {
-	t_list	*new_list;
-	t_list	*new_node;
+	int	count;
 
-	new_list = NULL;
+	count = 0;
 	while (lst)
 	{
-		new_node = ft_lstnew(f(lst->content));
-		if (!new_node)
-		{
-			ft_lstclear(&new_list, del);
-			return (NULL);
-		}
-		ft_lstadd_back(&new_list, new_node);
 		lst = lst->next;
+		count++;
 	}
-	return (new_list);
+	return (count);
 }
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -12,21 +12,25 @@
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
+	size_t	j;
 
-	if (*needle == '\0')
-		return ((char *)haystack);
-	while (len > 0)
+	if (little[0] == '\0')
+		return ((char *)big);
+	i = 0;
+	while (big[i] && i < len)
 	{
-		i = 0;
-		while (needle[i] && haystack[i] == needle[i])
-			i++;
-		if (needle[i] == '\0')
-			return ((char *)haystack);
-		haystack++;
-		len--;
+		j = 0;
+		while (little[j] && (i + j) < len
+			&& big[i + j] && big[i + j] == little[j])
+		{
+			j++;
+		}
+		if (little[j] == '\0')
+			return ((char *)&big[i]);
+		i++;
 	}
 	return (NULL);
 }
